59585f425ad798331d3873b23066be6de827f41d
zhuowenli <zhuowenligg@gmail.com>
2020-06-30 11:43:12 +0800

    :art: 优化代码

--- packages/miniapp-vue-devtools/src/main.js
+++ packages/miniapp-vue-devtools/src/main.js
@@ -14,7 +14,7 @@ const on = (event, callback) => {
     EventBus.push({ event, callback });
 };
 const emit = (event, data) => {
-    console.log('✅ emit', event, data);
+    console.log('📶 emit', event, data);
     my.sendSocketMessage({
         data: JSON.stringify([event, data])
     });
@@ -38,24 +38,7 @@ my.onSocketMessage(function (res) {
 const createSocket = () => {
     const host = target.__VUE_DEVTOOLS_HOST__ || 'http://localhost';
     const port = target.__VUE_DEVTOOLS_PORT__ !== undefined ? target.__VUE_DEVTOOLS_PORT__ : 8098;
-    const fullHost = port ? host + ':' + port : host;
-
-    my.connectSocket({
-        url: fullHost,
-        success(res) {
-            console.log('😀 createSocket success');
-            initBackend(bridge);
-            emit('vue-devtools-init');
-        },
-        fail(e) {
-            console.error('🚨 createSocket error', e);
-        }
-    });
-
-    on('vue-devtools-disconnect-backend', () => {
-        my.closeSocket();
-    });
-
+    const fullHost = `${port ? `${host}:${port}` : host}/socket.io/`;
     const bridge = new Bridge({
         listen(fn) {
             on('vue-message', data => fn(data));
@@ -65,7 +48,49 @@ const createSocket = () => {
         }
     });
 
-    installToast(target);
+    my.httpRequest({
+        url: fullHost.replace('ws', 'http'),
+        data: {
+            EIO: 3,
+            transport: 'polling',
+            t: 'NC32g82',
+        },
+        method: 'get',
+        success: (res) => {
+            console.log('🔗 createSocket', res.data);
+            if (/"sid":"(.+?)"/.test(res.data)) {
+                const sid = RegExp.$1;
+
+                my.connectSocket({
+                    url: fullHost,
+                    data: { EIO: 3, transport: 'websocket', sid },
+                    success(res) {
+                        console.log('🔗 createSocket success', res);
+                        console.log(fullHost);
+                    },
+                    fail(e) {
+                        console.error('🚨 createSocket error', e);
+                    }
+                });
+
+                my.onSocketOpen(function (res) {
+                    console.log('WebSocket 连接已打开！');
+
+                    initBackend(bridge);
+                    emit('vue-devtools-init');
+
+                    on('vue-devtools-disconnect-backend', () => {
+                        my.closeSocket();
+                    });
+
+                    installToast(target);
+                });
+            }
+        },
+        fail: (res) => {
+            console.error('🚨 fetch sid error', res);
+        },
+    });
 };
 
 createSocket();
